8396e9bd5578f9bb10c351c0a82ae39caa48894f
Sunil <memakiysunil69@gmail.com>
2026-05-17 01:11:03 +0530

    Fixed port configuration for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const app = require('./app');
 const mongoose = require('./config/db');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT,() =>{
     console.log(`listening on ${PORT}`);
