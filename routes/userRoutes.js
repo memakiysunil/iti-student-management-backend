659c7993db9a5153c5b143c1e868015c2d7a2ec1
Sunil <memakiysunil69@gmail.com>
2026-05-21 12:03:29 +0530

    final project update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const checkAdminRole = require('../middleware/adminMiddleware');
 const {jwtAuthmiddlewere} = require('../middleware/authMiddleware');
 
 router.post('/register',userController.register);
 router.post('/login', userController.login);
 router.get('/getprofile',jwtAuthmiddlewere,userController.getprofile);
 router.put('/updatePassword',jwtAuthmiddlewere,userController.updatePassword);
+router.get('/getalluser',jwtAuthmiddlewere,checkAdminRole,userController.getalluser);
 
 module.exports = router;
