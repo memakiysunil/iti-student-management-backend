85b62439a639fd0ff65a18a9834bdecda0e5807c
Sunil <memakiysunil69@gmail.com>
2026-05-21 11:36:54 +0530

    final project update

--- app.js
+++ app.js
@@ -7,7 +7,11 @@ const app = express();
 const userRouter = require('./routes/userRoutes');
  
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET','POST','PUT','DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.get('/', (req, res) => {
     res.json({
         success: true,
